32df925a2693d726de1f340ae100cb008a75ba53
Eliel Lopes Dos Santos <ellsantos@rd.com.br>
2023-01-22 21:22:52 -0300

    implementando funcionalidades de deletar itens e deletar tudo

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -10,11 +10,29 @@ import { TaskList } from '../../model/task-list';
 })
 export class TodoListComponent implements OnInit {
 
-  public taskList: Array<TaskList> = [];
+  public taskList: Array<TaskList> = [
+    { task: "Task 1", checked: true },
+    { task: "Task 2", checked: false },
+    { task: "Task 3", checked: false }
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public deleteItemTaskList(event: number) {
+    /* remove do array o elemento de index 'event' 
+    e apenas 1 elemento (no caso, o que foi passado) */
+    this.taskList.splice(event, 1); 
+  }
+
+  public deleteAllTaskList() {
+    const confirm = window.confirm("Você realmente deseja deletar todos os itens?");
+
+    if (confirm) {
+      this.taskList = [];
+    }
   }
 
 }
